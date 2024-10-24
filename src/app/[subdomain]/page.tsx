3ca8bcc8913b2fc3ec1d7d2e428b8253921ec264
samuelscn <samus355@gmail.com>
2024-10-24 17:39:57 -0300

    6 commit

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,13 +1,14 @@
-import { headers } from 'next/headers'
+'use server'
 
 function formatHostname(hostName: string) {
   const hostWithPort = hostName.split('.')[0]
   return hostWithPort.split(':')[0]
 }
 
-export default async function DynamicHome() {
-  const schoolName = process.env.NEXT_PUBLIC_NODE_ENV === 'local' ? 'teresiano' : formatHostname((await headers()).get('x-forwarded-host') as string)
-
+export default async function DynamicHome({ params }: { params: { subdomain: string } }) {
+  const { subdomain } = await params
+  const schoolName = process.env.NEXT_PUBLIC_NODE_ENV === 'local' ? 'teresiano' : subdomain
+  console.log(subdomain)
   return (
     <h1>{schoolName}</h1>
   );
