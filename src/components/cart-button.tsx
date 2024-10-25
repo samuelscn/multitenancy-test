09e76a1b1bb895ad6062b2cb1d4f22ebfd5ae178
samuelscn <samus355@gmail.com>
2024-10-25 19:22:00 -0300

    re

--- src/components/cart-button.tsx
+++ src/components/cart-button.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import { useCartStore } from "@/store/cart-store"
-
 export const CartButton = () => {
-  const { products } = useCartStore()
 
   return (
-    <span>Carrinho: {products.length}</span>
+    <span>Carrinho: {0}</span>
   )
 }
