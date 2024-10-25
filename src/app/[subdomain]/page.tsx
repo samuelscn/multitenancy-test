2404c9ca218d643771ee9e127cc9c24d18a77990
samuelscn <samus355@gmail.com>
2024-10-24 22:28:40 -0300

    get host

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,13 +1,26 @@
 'use server'
 
+import { headers } from "next/headers"
+
+function formatHostname(hostName: string) {
+  const hostWithPort = hostName.split('.')[0]
+  return hostWithPort.split(':')[0]
+}
+
 export default async function DynamicHome() {
-  const data = await fetch('https://aey7vvraaf.execute-api.us-east-1.amazonaws.com/dev/v2/schools/teresiano/activities?registered_student=true', { cache: 'force-cache' })
+  const dominioMap: any = {
+    'multitenancy-poc': 'teresiano',
+    'multitenancy-poc2': 'land-pituba'
+  }
+  const hostname = (await headers()).get('x-forwarded-host') as string
+  const nome = formatHostname(hostname) as any
+  const escola = process.env.NEXT_PUBLIC_NODE_ENV === 'local' ? 'teresiano' : dominioMap[nome]
+  const data = await fetch(`https://aey7vvraaf.execute-api.us-east-1.amazonaws.com/dev/v2/schools/${escola}?registered_student=true`, { cache: 'force-cache' })
   const activities = await data.json()
 
   return (
     <>
-      <h1>{activities.school_name}</h1>
-      {activities.activities.map((activity: any) => (<li>{activity.name}</li>))}
+      <h1>{JSON.stringify(activities)}</h1>
     </>
   );
 }
