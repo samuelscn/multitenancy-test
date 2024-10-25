9864c51d7cdf22a8add3d1eaeb5e803844b3c4e6
samuelscn <samus355@gmail.com>
2024-10-24 22:12:29 -0300

    add cache

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,7 +1,7 @@
 'use server'
 
 export default async function DynamicHome() {
-  const data = await fetch('https://aey7vvraaf.execute-api.us-east-1.amazonaws.com/dev/v2/schools/teresiano/activities?registered_student=true')
+  const data = await fetch('https://aey7vvraaf.execute-api.us-east-1.amazonaws.com/dev/v2/schools/teresiano/activities?registered_student=true', { cache: 'force-cache' })
   const activities = await data.json()
 
   return (
