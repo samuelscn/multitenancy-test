be9be1c42d4475530223512e9ad37f102f35980a
samuelscn <samus355@gmail.com>
2024-10-25 19:17:38 -0300

    remove store

--- src/components/add-product-button.tsx
+++ src/components/add-product-button.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import { useCartStore } from "@/store/cart-store"
-
 export const AddProductButton = () => {
-  const { addProduct } = useCartStore()
 
   return (
-    <button onClick={() => addProduct({ name: 'Capoeira' })}>Adicionar produto</button>
+    <button>Adicionar produto</button>
   )
 }
